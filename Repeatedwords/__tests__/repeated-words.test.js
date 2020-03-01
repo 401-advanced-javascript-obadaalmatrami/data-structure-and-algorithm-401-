88983149979f989a6160f4d18afc9106983cc884
obada almatrami <obada.matrami92@gmail.com>
2020-03-01 17:16:02 +0200

    finish

---
+++ Repeatedwords/__tests__/repeated-words.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const repeat = require('../src/repeated-words.js');
+describe('repeat function', () => {
+  it('returns first word that repeats in a string', () => {
+    const testStr = 'Once upon a time, there was a brave princess who...';
+    expect(repeat(testStr)).toEqual('a');
+    console.log('the first repeated word is : ', repeat(testStr));
+  });
+});
